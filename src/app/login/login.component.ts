0716c966379a507a5a109315924da642541a5052
francisco luna <fluna8@toromail.csudh.edu>
2024-06-23 03:21:24 -0700

    yeah

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onLogin() {
+    this.authService.login(this.email, this.password).then(
+      () => {
+        this.router.navigate(['']);
+      },
+      (error) => {
+        this.errorMessage = 'Credentials are incorrect or not registered';
+      }
+    );
+  }
+}
