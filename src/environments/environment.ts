cf7e89c17e904e86cd2212b495ba094d3ff3d7d7
Francisco <168025566+Francisco-111@users.noreply.github.com>
2025-03-18 22:23:28 -0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,12 +2,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDklFAKD5pjIiQEWhbaJPq-idasV0DXHqg",
-    authDomain: "p-a600f.firebaseapp.com",
-    projectId: "p-a600f",
-    storageBucket: "p-a600f.appspot.com",
-    messagingSenderId: "612141058253",
-    appId: "1:612141058253:web:0db0fefea0bb7ea2f5c184",
-    measurementId: "G-CGY8GS3B6B"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
